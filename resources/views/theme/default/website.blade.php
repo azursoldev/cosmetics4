150c6fed75a7e4e29b820ed94fa3d5d44e129adf
sohaibshaikh-dev <azursoldev@gmail.com>
2025-12-19 23:38:17 +0530

    home page created

--- resources/views/theme/default/website.blade.php
+++ resources/views/theme/default/website.blade.php
@@ -19,7 +19,7 @@
 	<link rel="icon" type="image/png" href="{{ get_favicon() }}">
 
 	<!-- Web Font -->
-	<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 	
 	<!-- StyleSheet -->
 	
